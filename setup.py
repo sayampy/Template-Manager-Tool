4c628c6950d733c24c7e7f9302afd8276956f354
sayampy <81681676+sayampy@users.noreply.github.com>
2021-08-17 12:48:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='templater-cli',
-    version='0.1',
+    version='0.3',
     author='Sayam.py',
     author_email='sujata.howrah.belgachia@gmail.com',
     description='Template Managing Tool',
